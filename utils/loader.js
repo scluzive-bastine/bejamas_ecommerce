fc61a84e4de43b9766ad5b23c6ba1f06f7f8a4d0
Sabastine <toshfuture@gmail.com>
2022-04-25 08:07:11 +0100

    mobile loader added

--- utils/loader.js
+++ utils/loader.js
@@ -2,57 +2,100 @@ import ContentLoader from 'react-content-loader'
 
 export const FeaturedLoader = (props) => {
   return (
-    <ContentLoader
-      speed={2}
-      width={'100%'}
-      height={'100%'}
-      viewBox="0 0 1607 964"
-      backgroundColor="#f3f3f3"
-      foregroundColor="#DFDFDF"
-      {...props}
-    >
-      <path d="M 0 14 h 339 v 38 H 0 z M 0 754 h 339 v 38 H 0 z M 1268 754 h 339 v 38 h -339 z M 1358 0 h 249 v 65 h -249 z" />
-      <path d="M 0 92 h 1607 v 622 H 0 z" />
-      <rect x="0" y="818" rx="7" ry="7" width="686" height="13" />
-      <rect x="0" y="843" rx="7" ry="7" width="625" height="13" />
-      <rect x="0" y="868" rx="7" ry="7" width="542" height="13" />
-      <rect x="0" y="893" rx="7" ry="7" width="360" height="13" />
-      <rect x="0" y="918" rx="7" ry="7" width="480" height="13" />
-      <rect x="0" y="943" rx="7" ry="7" width="580" height="13" />
-      <path d="M 1212 814 h 117 v 150 h -117 z M 1351 814 h 117 v 150 h -117 z M 1490 814 h 117 v 150 h -117 z" />
-    </ContentLoader>
+    <>
+      <ContentLoader
+        speed={2}
+        width={'100%'}
+        height={'100%'}
+        viewBox="0 0 1607 964"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#DFDFDF"
+        {...props}
+        style={{ width: '100%' }}
+        className="hidden md:flex"
+      >
+        <path d="M 0 14 h 339 v 38 H 0 z M 0 754 h 339 v 38 H 0 z M 1268 754 h 339 v 38 h -339 z M 1358 0 h 249 v 65 h -249 z" />
+        <path d="M 0 92 h 1607 v 622 H 0 z" />
+        <rect x="0" y="818" rx="7" ry="7" width="686" height="13" />
+        <rect x="0" y="843" rx="7" ry="7" width="625" height="13" />
+        <rect x="0" y="868" rx="7" ry="7" width="542" height="13" />
+        <rect x="0" y="893" rx="7" ry="7" width="360" height="13" />
+        <rect x="0" y="918" rx="7" ry="7" width="480" height="13" />
+        <rect x="0" y="943" rx="7" ry="7" width="580" height="13" />
+        <path d="M 1212 814 h 117 v 150 h -117 z M 1351 814 h 117 v 150 h -117 z M 1490 814 h 117 v 150 h -117 z" />
+      </ContentLoader>
+
+      {/* Mobile Loader */}
+      <ContentLoader
+        speed={2}
+        width={'100%'}
+        height={'100%'}
+        viewBox="0 0 348 680"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#DFDFDF"
+        {...props}
+        className="block md:hidden"
+      >
+        <path d="M 2 0 h 274 v 23 H 2 z M 2 340 h 274 v 10 H 2 z M 2 358 h 148 v 10 H 2 z M 1 454 h 148 v 10 H 1 z M 1 636 h 148 v 10 H 1 z M 0 656 h 56 v 10 H 0 z M 0 670 h 67 v 10 H 0 z M 2 278 h 339 v 46 H 2 z M 2 39 h 339 v 223 H 2 z" />
+        <rect x="0" y="0" rx="3" ry="3" width="279" height="5" />
+        <rect x="0" y="0" rx="3" ry="3" width="254" height="5" />
+        <rect x="0" y="0" rx="3" ry="3" width="220" height="6" />
+        <rect x="0" y="0" rx="3" ry="3" width="146" height="5" />
+        <rect x="0" y="0" rx="3" ry="3" width="195" height="5" />
+        <rect x="0" y="0" rx="3" ry="3" width="236" height="5" />
+        <path d="M 1 480 h 103 v 132 H 1 z M 124 480 h 102 v 132 H 124 z M 245 480 h 103 v 132 H 245 z" />
+      </ContentLoader>
+    </>
   )
 }
 
 export const ProductsLoader = (props) => {
   return (
-    <ContentLoader
-      speed={2}
-      width={'100%'}
-      height={'100%'}
-      viewBox="0 0 1617 1297"
-      backgroundColor="#f3f3f3"
-      foregroundColor="#DFDFDF"
-      {...props}
-    >
-      <path d="M 1 39 h 339 v 38 H 1 z M 963 1259 h 339 v 38 H 963 z" />
-      <rect x="1" y="115" rx="5" ry="5" width="230" height="23" />
-      <rect x="43" y="176" rx="5" ry="5" width="137" height="23" />
-      <rect x="1" y="176" rx="5" ry="5" width="29" height="23" />
-      <rect x="43" y="210" rx="5" ry="5" width="137" height="23" />
-      <rect x="1" y="210" rx="5" ry="5" width="29" height="23" />
-      <rect x="43" y="244" rx="5" ry="5" width="188" height="23" />
-      <rect x="1" y="244" rx="5" ry="5" width="29" height="23" />
-      <rect x="43" y="278" rx="5" ry="5" width="107" height="23" />
-      <rect x="1" y="278" rx="5" ry="5" width="29" height="23" />
-      <rect x="43" y="312" rx="5" ry="5" width="180" height="23" />
-      <rect x="1" y="312" rx="5" ry="5" width="29" height="23" />
-      <rect x="42" y="346" rx="5" ry="5" width="158" height="23" />
-      <rect x="0" y="346" rx="5" ry="5" width="29" height="23" />
-      <rect x="42" y="383" rx="5" ry="5" width="137" height="23" />
-      <rect x="0" y="383" rx="5" ry="5" width="29" height="23" />
-      <path d="M 1278 39 h 339 v 38 h -339 z" />
-      <path d="M 1 0 h 1616 v 1 H 1 z M 648 178 h 291 v 400 H 648 z M 648 592 h 91 v 10 h -91 z M 648 634 h 38 v 10 h -38 z M 648 608 h 130 v 20 H 648 z M 648 698 h 291 v 400 H 648 z M 648 1112 h 91 v 10 h -91 z M 648 1154 h 38 v 10 h -38 z M 648 1128 h 130 v 20 H 648 z M 987 178 h 291 v 400 H 987 z M 987 592 h 91 v 10 h -91 z M 987 634 h 38 v 10 h -38 z M 987 608 h 130 v 20 H 987 z M 987 698 h 291 v 400 H 987 z M 987 1112 h 91 v 10 h -91 z M 987 1154 h 38 v 10 h -38 z M 987 1128 h 130 v 20 H 987 z M 1326 178 h 291 v 400 h -291 z M 1326 592 h 91 v 10 h -91 z M 1326 634 h 38 v 10 h -38 z M 1326 608 h 130 v 20 h -130 z M 1326 698 h 291 v 400 h -291 z M 1326 1112 h 91 v 10 h -91 z M 1326 1154 h 38 v 10 h -38 z M 1326 1128 h 130 v 20 h -130 z" />
-    </ContentLoader>
+    <>
+      <ContentLoader
+        speed={2}
+        width={'100%'}
+        height={'100%'}
+        viewBox="0 0 1617 1297"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#DFDFDF"
+        {...props}
+        className="hidden md:flex"
+      >
+        <path d="M 1 39 h 339 v 38 H 1 z M 963 1259 h 339 v 38 H 963 z" />
+        <rect x="1" y="115" rx="5" ry="5" width="230" height="23" />
+        <rect x="43" y="176" rx="5" ry="5" width="137" height="23" />
+        <rect x="1" y="176" rx="5" ry="5" width="29" height="23" />
+        <rect x="43" y="210" rx="5" ry="5" width="137" height="23" />
+        <rect x="1" y="210" rx="5" ry="5" width="29" height="23" />
+        <rect x="43" y="244" rx="5" ry="5" width="188" height="23" />
+        <rect x="1" y="244" rx="5" ry="5" width="29" height="23" />
+        <rect x="43" y="278" rx="5" ry="5" width="107" height="23" />
+        <rect x="1" y="278" rx="5" ry="5" width="29" height="23" />
+        <rect x="43" y="312" rx="5" ry="5" width="180" height="23" />
+        <rect x="1" y="312" rx="5" ry="5" width="29" height="23" />
+        <rect x="42" y="346" rx="5" ry="5" width="158" height="23" />
+        <rect x="0" y="346" rx="5" ry="5" width="29" height="23" />
+        <rect x="42" y="383" rx="5" ry="5" width="137" height="23" />
+        <rect x="0" y="383" rx="5" ry="5" width="29" height="23" />
+        <path d="M 1278 39 h 339 v 38 h -339 z" />
+        <path d="M 1 0 h 1616 v 1 H 1 z M 648 178 h 291 v 400 H 648 z M 648 592 h 91 v 10 h -91 z M 648 634 h 38 v 10 h -38 z M 648 608 h 130 v 20 H 648 z M 648 698 h 291 v 400 H 648 z M 648 1112 h 91 v 10 h -91 z M 648 1154 h 38 v 10 h -38 z M 648 1128 h 130 v 20 H 648 z M 987 178 h 291 v 400 H 987 z M 987 592 h 91 v 10 h -91 z M 987 634 h 38 v 10 h -38 z M 987 608 h 130 v 20 H 987 z M 987 698 h 291 v 400 H 987 z M 987 1112 h 91 v 10 h -91 z M 987 1154 h 38 v 10 h -38 z M 987 1128 h 130 v 20 H 987 z M 1326 178 h 291 v 400 h -291 z M 1326 592 h 91 v 10 h -91 z M 1326 634 h 38 v 10 h -38 z M 1326 608 h 130 v 20 h -130 z M 1326 698 h 291 v 400 h -291 z M 1326 1112 h 91 v 10 h -91 z M 1326 1154 h 38 v 10 h -38 z M 1326 1128 h 130 v 20 h -130 z" />
+      </ContentLoader>
+
+      {/* Mobile Loader */}
+      <ContentLoader
+        speed={2}
+        width={'100%'}
+        height={'100%'}
+        viewBox="0 0 348 1840"
+        backgroundColor="#f3f3f3"
+        foregroundColor="#DFDFDF"
+        {...props}
+        className="mt-10 block md:hidden"
+      >
+        <path d="M 1 0 h 347 v 476.976 H 1 z M 1 493.67 h 108.512 v 11.924 H 1 z M 1 543.753 h 45.313 v 11.924 H 1 z M 1 512.749 h 155.017 v 23.849 H 1 z M 0 581 h 347 v 476.976 H 0 z M 0 1074.67 h 108.512 v 11.924 H 0 z M 0 1124.75 h 45.313 v 11.924 H 0 z M 0 1093.75 h 155.017 v 23.849 H 0 z M 0 1174 h 347 v 476.976 H 0 z M 0 1667.67 h 108.512 v 11.924 H 0 z M 0 1717.75 h 45.313 v 11.924 H 0 z M 0 1686.75 h 155.017 v 23.849 H 0 z" />
+        <path d="M 5 1802 h 339 v 38 H 5 z" />
+      </ContentLoader>
+    </>
   )
 }
